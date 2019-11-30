5b37b8e537e15306823af5ed0e4e477a5d12bd41
n-is <073bex422.nischal@pcampus.edu.np>
2019-11-30 15:56:28 +0545

    Add test files.

---
+++ Tests/integrator/integrator_test.cpp
@@ -0,0 +1,38 @@
+#include "integrator.h"
+#include "intalgo.h"
+
+#include "catch2/catch.hpp"
+
+class dummyAlgo: public Integration_Algorithm
+{
+public:
+        dummyAlgo() { }
+        dummyAlgo(dummyAlgo &&) = default;
+        dummyAlgo(const dummyAlgo &) = default;
+        dummyAlgo &operator=(dummyAlgo &&) = default;
+        dummyAlgo &operator=(const dummyAlgo &) = default;
+        ~dummyAlgo() { }
+
+        float integrate(float dx, float y) {
+                return dx*y;
+        } 
+};
+
+template<typename T, size_t N>
+constexpr size_t len(T (&arr)[N]) {
+        return N;
+}
+
+TEST_CASE("Testing Integrator class", "[integration]") {
+
+        dummyAlgo dum;
+
+        Integrator sum(&dum);
+
+        float vals[] = {1.1, 1.2, 1.3, 1.4};
+
+        for (size_t i = 0; i < len(vals); ++i) {
+                float v = sum.integrate(i, vals[i]);
+                REQUIRE_THAT(v, Catch::Matchers::WithinRel(i*vals[i]));
+        }
+}
